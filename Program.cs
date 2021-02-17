eb9a8e4d5b5ce70fd16349ff08a067188dae800c
keylato <78444133+keylato@users.noreply.github.com>
2021-02-17 08:46:43 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorldInverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] array = new string[12] {"H","e","l","l","o"," ","W","o","r","l","d","!"};
+
+             for (int i=12; i > -1; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+    
+        }
+    }
+}
